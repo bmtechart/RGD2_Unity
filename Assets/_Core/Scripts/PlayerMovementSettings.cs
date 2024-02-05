12258c1a4086640318b558ef0f8c5a3c5032195f
rockyleishman <rockyleishman@gmail.com>
2024-02-05 16:10:40 -0700

    Added scriptable object for player movement
    Added PlayerMovementSettings & PlayerMovementController. The new PlayerMovementController is copied from the Starter Assets FirstPersonController; the only changes are to allow scriptable objects to hold the parameters.

---
+++ Assets/_Core/Scripts/PlayerMovementSettings.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerMovementSettings", menuName = "Scriptable Objects/PlayerMovementSettings", order = 0)]
+public class PlayerMovementSettings : ScriptableObject
+{
+	[Header("Player")]
+	[Tooltip("Move speed of the character in m/s")]
+	public float MoveSpeed = 4.0f;
+	[Tooltip("Sprint speed of the character in m/s")]
+	public float SprintSpeed = 6.0f;
+	[Tooltip("Rotation speed of the character")]
+	public float RotationSpeed = 1.0f;
+	[Tooltip("Acceleration and deceleration")]
+	public float SpeedChangeRate = 10.0f;
+
+	[Space(10)]
+	[Tooltip("The height the player can jump")]
+	public float JumpHeight = 1.2f;
+	[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
+	public float Gravity = -15.0f;
+
+	[Space(10)]
+	[Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+	public float JumpTimeout = 0.1f;
+	[Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
+	public float FallTimeout = 0.15f;
+
+	[Header("Player Grounded")]
+	[Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
+	public bool Grounded = true;
+	[Tooltip("Useful for rough ground")]
+	public float GroundedOffset = -0.14f;
+	[Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+	public float GroundedRadius = 0.5f;
+	[Tooltip("What layers the character uses as ground")]
+	public LayerMask GroundLayers;
+
+	[Header("Cinemachine")]
+	[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+	public GameObject CinemachineCameraTarget;
+	[Tooltip("How far in degrees can you move the camera up")]
+	public float TopClamp = 90.0f;
+	[Tooltip("How far in degrees can you move the camera down")]
+	public float BottomClamp = -90.0f;
+}
