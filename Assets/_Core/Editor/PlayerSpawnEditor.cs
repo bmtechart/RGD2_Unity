fba73d38e7fbb789ca016931eb96cdd435fcb3d9
Brian Murphy <brianmurphy@bmtechart.com>
2024-02-12 01:43:09 -0700

    added player spawner prefab

---
+++ Assets/_Core/Editor/PlayerSpawnEditor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PlayerSpawner))]
+public class PlayerSpawnEditor : Editor
+{
+    //shows when component is selected
+    private void OnSceneGUI()
+    {
+        PlayerSpawner playerSpawner = (PlayerSpawner)target;
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(playerSpawner.transform.position, playerSpawner.transform.up, 1f);
+    }
+
+
+
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        PlayerSpawner playerSpawner = (PlayerSpawner)target;
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(playerSpawner.transform.position, playerSpawner.transform.up, 1.5f);
+        Handles.Label(playerSpawner.transform.position, "PlayerSpawn");
+    }
+}
---
+++ Assets/_Core/Scripts/PlayerSpawner.cs
@@ -0,0 +1,34 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerCapsule;
+    [SerializeField] private GameObject MainCamera;
+    [SerializeField] private GameObject PlayerFollowCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerCapsule = Instantiate(PlayerCapsule, transform);
+        MainCamera = Instantiate(MainCamera, transform);
+        PlayerFollowCamera = Instantiate(PlayerFollowCamera, transform);
+
+        CinemachineVirtualCamera virtualFollowCamera = PlayerFollowCamera.GetComponent<CinemachineVirtualCamera>();
+        virtualFollowCamera.Follow = PlayerCapsule.transform.Find("PlayerCameraRoot");
+        virtualFollowCamera.LookAt = PlayerCapsule.transform.Find("Player");
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 gizmoCenter = transform.position + new Vector3(0, 1, 0);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(gizmoCenter, new Vector3(1, 2, 1));
+
+        Gizmos.DrawLine(gizmoCenter, (transform.forward * 1.0f) + gizmoCenter);
+        Gizmos.DrawSphere((transform.forward * 1.0f) + gizmoCenter, 0.1f);
+        Handles.Label(gizmoCenter, "Player Spawn");
+    }
+}
